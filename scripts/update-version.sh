9d3891ef213e562192889a13ba97418a5ac2c6cd
marc.sirisak <marc.sirisak@beta.gouv.fr>
2025-03-17 18:33:51 +0100

    feat: add updater in tauri.conf and update-versions script

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -22,3 +22,18 @@ sed -i 's/"version": "'$current_version'"/"version": "'$new_version'"/' src-taur
 jq '.version = "'"$new_version"'"' package.json > package.json.tmp && mv package.json.tmp package.json
 
 echo "Version updated from $current_version to $new_version"
+
+
+# Update endpoint version  for updater plugin
+CONFIG_FILE="./src-tauri/tauri.conf.json"  # Path to your tauri.conf.json file
+# Find the download URL for the matching asset
+DOWNLOAD_URL="https://github.com/tchapgouv/tchap-desktop/releases/download/tchap-${new_version}/tchap-desktop_${new_version}_x64-setup-debug.exe"
+
+echo "Found download URL: $DOWNLOAD_URL"
+
+jq --arg url "$DOWNLOAD_URL" '.plugins.updater.endpoints = [$url]' "$CONFIG_FILE"
+if [ $? -eq 0 ]; then
+    echo "Successfully updated $CONFIG_FILE with jq"
+else
+    echo "Error updating ${CONFIG_FILE} with jq"
+fi
