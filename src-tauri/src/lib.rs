d16a8dac915d236a3f4857a620aca3f7d116202f
marc.sirisak <marc.sirisak@beta.gouv.fr>
2025-07-11 12:20:35 +0200

    feat: add menu to tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,6 +13,8 @@ use seshat::Database;
 use tauri::Manager;
 use tauri_plugin_deep_link::DeepLinkExt;
 use tauri::tray::TrayIconBuilder;
+use tauri::menu::{MenuBuilder, MenuItemBuilder};
+
 #[derive(Clone)]
 pub struct MyState {
     pub database: Option<Arc<Mutex<Database>>>,
@@ -104,8 +106,32 @@ pub fn run() {
 
 
             // Create the tray icon
+            let quit = MenuItemBuilder::new("Quit").id("quit").build(app).unwrap();
+            let hide = MenuItemBuilder::new("Hide").id("hide").build(app).unwrap();
+            let show = MenuItemBuilder::new("Show").id("show").build(app).unwrap();
+
+            let menu = MenuBuilder::new(app)
+              .items(&[&quit, &hide, &show])
+              .build()
+              .unwrap();
+           
             TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .on_menu_event(|app, event| match event.id().as_ref() {
+                    "quit" => app.exit(0),
+                    "hide" => {
+                      dbg!("menu item hide clicked");
+                      let window = app.get_webview_window("main").unwrap();
+                      window.hide().unwrap();
+                    }
+                    "show" => {
+                      dbg!("menu item show clicked");
+                      let window = app.get_webview_window("main").unwrap();
+                      window.show().unwrap();
+                    }
+                    _ => {}
+                  })
                 .build(app)?;
 
             Ok(())
