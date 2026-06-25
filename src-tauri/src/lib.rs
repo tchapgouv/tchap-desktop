f8c13a4ab7d9428017d78a01047109d08058ee3a
marc.sirisak <marc.sirisak@beta.gouv.fr>
2026-06-25 16:09:28 +0200

    Update Cargo dependencies and remove unused tauri upload plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -65,7 +65,6 @@ pub fn run() {
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_upload::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_persisted_scope::init())
         .plugin(tauri_plugin_dialog::init())
