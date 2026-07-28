92a45acd9ef3550520b5f816706882f0d7462b72
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-07-28 18:18:13 +0200

    packaging: add Nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Tchap desktop client";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          tchap-desktop = pkgs.callPackage ./nix/package.nix { };
+          default = tchap-desktop;
+        }
+      );
+    };
+}
